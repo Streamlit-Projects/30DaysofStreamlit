7fc6fec0d9f2fe873aa5ff6ca1369cc4ec62ed0a
Martina Bohunicka-Charles <105567611+mBohunickaCharles@users.noreply.github.com>
2023-01-02 17:24:44 +0000

    Update app.py

--- Day_4/app.py
+++ Day_4/app.py
@@ -37,7 +37,7 @@ def audience_simple(country):
 @st.cache
 def load_data():
     """ Loads in 4 dataframes and does light feature engineering"""
-    df_agg = pd.read_csv('data/Aggregated_Metrics_By_Video.csv').iloc[1:, :]
+    df_agg = pd.read_csv('..data/Aggregated_Metrics_By_Video.csv').iloc[1:, :]
     df_agg.columns = ['Video', 'Video title', 'Video publish time', 'Comments added', 'Shares', 'Dislikes', 'Likes',
                       'Subscribers lost', 'Subscribers gained', 'RPM(USD)', 'CPM(USD)', 'Average % viewed',
                       'Average view duration',
@@ -51,9 +51,9 @@ def load_data():
         'Likes']) / df_agg.Views
     df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
     df_agg.sort_values('Video publish time', ascending=False, inplace=True)
-    df_agg_sub = pd.read_csv('data/Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
-    df_comments = pd.read_csv('data/Aggregated_Metrics_By_Video.csv')
-    df_time = pd.read_csv('data/Video_Performance_Over_Time.csv')
+    df_agg_sub = pd.read_csv('..data/Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
+    df_comments = pd.read_csv('..data/Aggregated_Metrics_By_Video.csv')
+    df_time = pd.read_csv('..data/Video_Performance_Over_Time.csv')
     df_time['Date'] = pd.to_datetime(df_time['Date'])
     return df_agg, df_agg_sub, df_comments, df_time
 
